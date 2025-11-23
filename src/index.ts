274dcd20188662227d6da33f139b0a4c779df9d0
JaBreaK <114270384+JaBreaK@users.noreply.github.com>
2025-11-23 14:00:15 +0700

    Add new allowed origins for CORS

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   }
   next();
 });
-const allowedOrigins = ['https://bellonime.web.id', 'https://same.bellonime.web.id'];
+const allowedOrigins = ['https://bellonime.web.id', 'https://same.bellonime.web.id', 'https://bello.web.id', 'http://bello.web.id'];
 
 const corsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
